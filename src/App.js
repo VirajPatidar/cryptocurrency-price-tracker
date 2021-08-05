227ab2495338c79a3f237104f6b29c4a77a11c94
Viraj Patidar <vrj381@gmail.com>
2021-08-05 14:57:23 +0530

    axios setup + api called

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+            )
+            .then(res => {
+                setCoins(res.data);
+                console.log(res.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
+
+
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    );
 }
 
 export default App;
