34e9bf8f594ddaedf3248e51d7db60c482d19fcd
Viraj Patidar <vrj381@gmail.com>
2021-08-05 15:51:08 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ function App() {
 
     useEffect(() => {
         axios
-            .get(
-                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-            )
+            .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false')
             .then(res => {
                 setCoins(res.data);
-                console.log(res.data);
             })
             .catch(error => console.log(error));
     }, []);
